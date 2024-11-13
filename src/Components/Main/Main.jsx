0703737ad4fc68f3f0a07f40111f6d7c454bc40e
Agleice Faria de Sousa <agleicesousa98@gmail.com>
2024-11-13 14:56:06 -0300

    first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+    return (
+        <>
+            <section>
+                <h1>Olá, mundo!</h1>
+            </section>
+        </>
+    )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <section>
+        <h1>Deu certo!</h1>
+      </section>
+    </>
+  );
+}
