befe3d8e03fe102d35311f43755a69edf7fcdfc9
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-15 06:27:01 +0000

    refactor: add email em senhas em todos os usuarios.

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.ts?$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'js'],
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.json',
-      },
-    },
-  };  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
+};
--- src/entities/adminEntities.ts
+++ src/entities/adminEntities.ts
@@ -1,18 +1,9 @@
-import {
-  Entity,
-  Column,
-  BeforeInsert,
-  BeforeUpdate,
-  OneToOne,
-  JoinColumn,
-  OneToMany
-} from 'typeorm';
+import { Entity, Column, OneToOne, JoinColumn, OneToMany } from 'typeorm';
 import { BaseEntity } from './baseEntity';
 import { Membros } from './membrosEntities';
 import { Turma } from './turmasEntities';
 import { Professor } from './professorEntities';
 import { Alunos } from './alunosEntities';
-import { criptografarSenha } from '../utils/senhaUtils';
 
 @Entity()
 export class Admin extends BaseEntity {
@@ -23,20 +14,6 @@ export class Admin extends BaseEntity {
   @OneToOne(() => Membros, { eager: true })
   @JoinColumn({ name: 'membroId' })
   membro: Membros;
-
-  /**
-   * E-mail único do administrador.
-   * @type {string}
-   */
-  @Column({ unique: true })
-  email: string;
-
-  /**
-   * Senha do administrador, armazenada de forma criptografada.
-   * @type {string}
-   */
-  @Column()
-  senha: string;
 
   /**
    * Relação com as turmas gerenciadas pelo administrador.
@@ -52,26 +29,4 @@ export class Admin extends BaseEntity {
 
   @OneToMany(() => Alunos, (aluno) => aluno.admin)
   alunos: Alunos[];
-
-  @BeforeInsert()
-  @BeforeUpdate()
-
-  /**
-   * Antes de inserir ou atualizar um registro de administrador, criptografa a senha.
-   */
-  @BeforeInsert()
-  @BeforeUpdate()
-  async handleCriptografiaSenha(): Promise<void> {
-    if (this.senha && this.isSenhaPlainText()) {
-      this.senha = await criptografarSenha(this.senha);
-    }
-  }
-
-  /**
-   * Verifica se a senha está em formato de texto puro.
-   * @returns {boolean} true se a senha estiver em texto puro, false caso contrário.
-   */
-  private isSenhaPlainText(): boolean {
-    return !this.senha.startsWith('$2b$'); // Hash bcrypt começa com $2b$
-  }
 }
