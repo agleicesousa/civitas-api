42b66b47c04e0f51f339609479b34fd724514fb2
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-15 09:38:29 +0000

    Remove: código adminEntities duplicado

--- src/entities/adminEntities.ts
+++ src/entities/adminEntities.ts
@@ -23,29 +23,4 @@ export class Admin extends BaseEntity {
 
   @OneToMany(() => Alunos, (aluno) => aluno.admin)
   alunos: Alunos[];
-}import { Entity, OneToOne, JoinColumn, OneToMany } from 'typeorm';
-import { BaseEntity } from './baseEntity';
-import { Membros } from './membrosEntities';
-import { Turma } from './turmasEntities';
-import { Professor } from './professorEntities';
-import { Alunos } from './alunosEntities';
-
-@Entity()
-export class Admin extends BaseEntity {
-  /**
-   * Relacionamento com a entidade `Membros`, indicando que um administrador é também um membro.
-   * @type {Membros}
-   */
-  @OneToOne(() => Membros, { eager: true })
-  @JoinColumn({ name: 'membroId' })
-  membro: Membros;
-
-  @OneToMany(() => Turma, (turma) => turma.admin)
-  turmas: Turma[];
-
-  @OneToMany(() => Professor, (professor) => professor.admin)
-  professores: Professor[];
-
-  @OneToMany(() => Alunos, (aluno) => aluno.admin)
-  alunos: Alunos[];
 }
