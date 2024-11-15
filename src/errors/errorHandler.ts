d4814189aa0853272874f254d1ecc4a4c897819b
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-15 17:53:52 +0000

    refactor: prettier

--- src/errors/errorHandler.ts
+++ src/errors/errorHandler.ts
@@ -1,11 +1,11 @@
 export default class ErrorHandler extends Error {
   statusCode: number;
-  
+
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
   }
-  
+
   static badRequest(message: string) {
     return new ErrorHandler(message, 400);
   }
