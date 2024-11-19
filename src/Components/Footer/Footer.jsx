b46eff4d628b5658053cf6ad29e275f5458797e1
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-19 11:58:23 +0000

    fix: ajuste rotas

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
         <footer>
-            <p>Copyright © 2022 VNW React Aula. All rights reserved.</p>
+            <p>Copyright © 2024 VNW React Aula. All rights reserved.</p>
         </footer>
         </>
     );
