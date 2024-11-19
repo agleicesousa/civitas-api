d9c90b4d0f3b3878c8b6ee3b04ad2de48151c827
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-19 03:26:24 +0000

    refactor: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import Footer from './Components/Footer/Footer.jsx';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Header from './Components/Header/Header.jsx'
+import Home from './Pages/Home/Home.jsx'
+import Footer from './Components/Footer/Footer.jsx'
+
 import './Components/GlobalStyle/globalStyle.scss'
 
 export default function App() {
   return (
-    <section>
+    <BrowserRouter>
       <Header />
+      <Route>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Route>
       <Footer />
-    </section>
+    </BrowserRouter>
   );
 }
