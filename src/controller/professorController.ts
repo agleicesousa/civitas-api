47b4f2ea49bde6294c773ccbc189f7be23d703ad
agleicesousa <agleicesousa97@gmail.com>
2024-11-21 16:18:35 -0300

    feat/remove: add email e senha na criação e removendo swagger

--- src/controller/professorController.ts
+++ src/controller/professorController.ts
@@ -1,16 +1,10 @@
 import { Request, Response } from 'express';
 import { ProfessorService } from '../services/professorService';
-import { AppError } from '../utils/appError';
+import ErrorHandler from '../errors/errorHandler';
 
-/**
- * Controlador para gerenciar os professores.
- */
 export class ProfessorController {
   private professorService = new ProfessorService();
 
-  /**
-   * Cria um novo professor e o associa ao admin que está criando.
-   */
   async criarProfessor(req: Request, res: Response) {
     const {
       nomeMembro,
@@ -18,6 +12,9 @@ export class ProfessorController {
       dataNascimento,
       numeroMatricula,
       turmasApelido,
+      email,
+      senha,
+      turmaIds,
     } = req.body;
     const adminId = req.user.id;
 
@@ -28,20 +25,20 @@ export class ProfessorController {
         dataNascimento,
         numeroMatricula,
         turmasApelido,
-        adminId
+        email,
+        senha,
+        adminId,
+        turmaIds
       );
       return res.status(201).json(professor);
     } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).json({ error: error.message });
+      if (error instanceof ErrorHandler) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
-      return res.status(500).json({ error: 'Erro interno do servidor' });
+      return res.status(500).json({ message: 'Erro interno do servidor' });
     }
   }
 
-  /**
-   * Lista todos os professores associados ao admin autenticado.
-   */
   async listarProfessores(req: Request, res: Response) {
     const adminId = req.user.id;
 
@@ -49,16 +46,13 @@ export class ProfessorController {
       const professores = await this.professorService.listarProfessores(adminId);
       return res.status(200).json(professores);
     } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).json({ error: error.message });
+      if (error instanceof ErrorHandler) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
-      return res.status(500).json({ error: 'Erro interno do servidor' });
+      return res.status(500).json({ message: 'Erro interno do servidor' });
     }
   }
 
-  /**
-   * Busca um professor pelo ID, somente se o admin que está fazendo a requisição for o mesmo que o criou.
-   */
   async buscarProfessorPorId(req: Request, res: Response) {
     const professorId = parseInt(req.params.id);
     const adminId = req.user.id;
@@ -70,40 +64,35 @@ export class ProfessorController {
       );
       return res.status(200).json(professor);
     } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).json({ error: error.message });
+      if (error instanceof ErrorHandler) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
-      return res.status(500).json({ error: 'Erro interno do servidor' });
+      return res.status(500).json({ message: 'Erro interno do servidor' });
     }
   }
 
-  /**
-   * Edita os dados de um professor, somente se o admin que está fazendo a requisição for o mesmo que o criou.
-   */
   async editarProfessor(req: Request, res: Response) {
     const professorId = parseInt(req.params.id);
     const adminId = req.user.id;
-    const { turmasApelido, membroId } = req.body;
+    const { email, senha, turmaIds } = req.body;
 
     try {
       const professor = await this.professorService.editarProfessor(
         professorId,
         adminId,
-        turmasApelido,
-        membroId
+        email,
+        senha,
+        turmaIds
       );
       return res.status(200).json(professor);
     } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).json({ error: error.message });
+      if (error instanceof ErrorHandler) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
-      return res.status(500).json({ error: 'Erro interno do servidor' });
+      return res.status(500).json({ message: 'Erro interno do servidor' });
     }
   }
 
-  /**
-   * Deleta um professor, somente se o admin que está fazendo a requisição for o mesmo que o criou.
-   */
   async deletarProfessor(req: Request, res: Response) {
     const professorId = parseInt(req.params.id);
     const adminId = req.user.id;
@@ -112,10 +101,10 @@ export class ProfessorController {
       await this.professorService.deletarProfessor(professorId, adminId);
       return res.status(204).send();
     } catch (error) {
-      if (error instanceof AppError) {
-        return res.status(error.statusCode).json({ error: error.message });
+      if (error instanceof ErrorHandler) {
+        return res.status(error.statusCode).json({ message: error.message });
       }
-      return res.status(500).json({ error: 'Erro interno do servidor' });
+      return res.status(500).json({ message: 'Erro interno do servidor' });
     }
   }
 }
--- src/services/professorService.ts
+++ src/services/professorService.ts
@@ -1,134 +1,92 @@
-import { Professor } from '../entities/professoresEntities';
+import { Professor } from '../entities/professorEntities';
 import { Membros } from '../entities/membrosEntities';
 import { Turma } from '../entities/turmasEntities';
 import { Admin } from '../entities/adminEntities';
-import { AppError } from '../utils/appError';
+import ErrorHandler from '../errors/errorHandler';
 import { TipoConta } from '../entities/baseEntity';
+import { criptografarSenha } from '../utils/senhaUtils';
 
 export class ProfessorService {
-  /**
-   * Cria um novo professor e associa a ele múltiplas turmas.
-   * 
-   * @param nomeMembro - Nome do membro.
-   * @param cpf - CPF do membro.
-   * @param dataNascimento - Data de nascimento do membro.
-   * @param numeroMatricula - Número de matrícula do professor.
-   * @param turmasApelido - Apelido das turmas associadas ao professor.
-   * @param adminId - ID do administrador que está criando o professor.
-   * @param turmaIds - IDs das turmas que serão associadas ao professor.
-   * @returns O professor criado com as turmas associadas.
-   * 
-   * @throws AppError - Se ocorrer um erro ao criar o professor.
-   */
+
   async criarProfessor(
     nomeMembro: string,
     cpf: string,
     dataNascimento: Date,
     numeroMatricula: string,
     turmasApelido: string,
+    email: string,
+    senha: string,
     adminId: number,
     turmaIds: number[]
   ): Promise < Professor > {
-    const membro = new Membros();
-    membro.nome = nomeMembro;
-    membro.cpf = cpf;
-    membro.dataNascimento = dataNascimento;
-    membro.tipoConta = TipoConta.PROFESSOR;
-
-    // Criação do membro
-    await membro.save();
-
-    // Busca as turmas com base nos IDs
-    const turmas = await Turma.findByIds(turmaIds);
-
-    // Verifica se todas as turmas foram encontradas
-    if (turmas.length !== turmaIds.length) {
-      throw new AppError('Algumas turmas não foram encontradas', 404);
+    // Verifica se o e-mail já está registrado
+    const membroExistente = await Membros.findOne({ where: { email } });
+    if (membroExistente) {
+      throw ErrorHandler.badRequest('Email já registrado');
     }
 
+    // Cria o novo membro
+    const membro = new Membros();
+    membro.nomeCompleto = nomeMembro;
+    membro.cpf = cpf;
+    membro.dataNascimento = dataNascimento;
+    membro.numeroMatricula = numeroMatricula;
+    membro.email = email;
+    membro.senha = await criptografarSenha(senha);
+    membro.tipoConta = TipoConta.PROFESSOR;
+
+    await membro.save();
+
+    // Verifica se todas as turmas existem
+    const turmas = await Turma.findByIds(turmaIds);
+    if (turmas.length !== turmaIds.length) {
+      throw ErrorHandler.notFound('Algumas turmas não foram encontradas');
+    }
+
+    // Cria o novo professor
     const professor = new Professor();
     professor.membro = membro;
     professor.turmas = turmas;
-    professor.turmasApelido = turmasApelido;
-    professor.admin = { id: adminId } as Admin;
+    professor.adminId = adminId;
 
     await professor.save();
 
     return professor;
   }
 
-  /**
-   * Lista todos os professores de um admin específico.
-   * 
-   * @param adminId - ID do administrador para filtrar os professores.
-   * @returns Lista de professores do admin.
-   */
-  async listarProfessores(adminId: number): Promise < Professor[] > {
-    return Professor.find({ where: { admin: { id: adminId } }, relations: ['membro', 'admin', 'turmas'] });
-  }
-
-  /**
-   * Busca um professor pelo ID, verificando se o admin é o mesmo que o criou.
-   * 
-   * @param professorId - ID do professor a ser buscado.
-   * @param adminId - ID do admin que está buscando o professor.
-   * @returns O professor encontrado.
-   * 
-   * @throws AppError - Se o professor não for encontrado ou se o admin não for o responsável por ele.
-   */
-  async buscarProfessorPorId(professorId: number, adminId: number): Promise < Professor > {
-    const professor = await Professor.findOne({ where: { id: professorId }, relations: ['admin', 'turmas'] });
-
-    if (!professor) {
-      throw new AppError('Professor não encontrado', 404);
-    }
-
-    if (professor.admin.id !== adminId) {
-      throw new AppError('Você não tem permissão para acessar esse professor', 403);
-    }
-
-    return professor;
-  }
-
-  /**
-   * Edita um professor, atualizando as turmas associadas ao professor.
-   * 
-   * @param professorId - ID do professor a ser editado.
-   * @param adminId - ID do admin que está editando o professor.
-   * @param turmasApelido - Apelido das turmas a ser atualizado.
-   * @param membroId - ID do membro a ser atualizado.
-   * @param turmaIds - IDs das turmas a serem associadas ao professor.
-   * @returns O professor atualizado.
-   * 
-   * @throws AppError - Se o professor não for encontrado ou se o admin não for o responsável por ele.
-   */
   async editarProfessor(
     professorId: number,
     adminId: number,
-    turmasApelido: string,
-    membroId: number,
+    email: string,
+    senha: string,
     turmaIds: number[]
   ): Promise < Professor > {
-    const professor = await Professor.findOne({ where: { id: professorId }, relations: ['admin', 'membro', 'turmas'] });
+    // Busca o professor com base no ID e nas relações com admin, membro e turmas
+    const professor = await Professor.findOne({
+      where: { id: professorId },
+      relations: ['admin', 'membro', 'turmas']
+    });
 
     if (!professor) {
-      throw new AppError('Professor não encontrado', 404);
+      throw ErrorHandler.notFound('Professor não encontrado');
     }
 
+    // Verifica se o admin que está fazendo a edição é o mesmo que criou o professor.
     if (professor.admin.id !== adminId) {
-      throw new AppError('Você não tem permissão para editar esse professor', 403);
+      throw ErrorHandler.forbidden('Você não tem permissão para editar esse professor');
     }
 
-    // Atualiza o apelido das turmas e o membro
-    professor.turmasApelido = turmasApelido;
-    professor.membro.id = membroId;
+    if (email) {
+      professor.membro.email = email;
+    }
 
-    // Atualiza as turmas associadas ao professor
+    if (senha) {
+      professor.membro.senha = await criptografarSenha(senha);
+    }
+
     const turmas = await Turma.findByIds(turmaIds);
-
-    // Verifica se todas as turmas foram encontradas
     if (turmas.length !== turmaIds.length) {
-      throw new AppError('Algumas turmas não foram encontradas', 404);
+      throw ErrorHandler.notFound('Algumas turmas não foram encontradas');
     }
 
     professor.turmas = turmas;
@@ -138,22 +96,34 @@ export class ProfessorService {
     return professor;
   }
 
-  /**
-   * Deleta um professor, apenas se o admin for o mesmo que o criou.
-   * 
-   * @param professorId - ID do professor a ser deletado.
-   * @param adminId - ID do admin que está deletando o professor.
-   * @throws AppError - Se o professor não for encontrado ou se o admin não for o responsável por ele.
-   */
-  async deletarProfessor(professorId: number, adminId: number): Promise < void > {
-    const professor = await Professor.findOne({ where: { id: professorId }, relations: ['admin'] });
+  async listarProfessores(adminId: number): Promise < Professor[] > {
+    const professores = await Professor.find({ where: { adminId }, relations: ['membro', 'turmas'] });
 
-    if (!professor) {
-      throw new AppError('Professor não encontrado', 404);
+    if (professores.length === 0) {
+      throw ErrorHandler.notFound('Nenhum professor encontrado');
     }
 
-    if (professor.admin.id !== adminId) {
-      throw new AppError('Você não tem permissão para deletar esse professor', 403);
+    return professores;
+  }
+
+  async buscarProfessorPorId(professorId: number, adminId: number): Promise < Professor > {
+    const professor = await Professor.findOne({
+      where: { id: professorId, adminId },
+      relations: ['membro', 'turmas']
+    });
+
+    if (!professor) {
+      throw ErrorHandler.notFound('Professor não encontrado');
+    }
+
+    return professor;
+  }
+  
+  async deletarProfessor(professorId: number, adminId: number): Promise < void > {
+    const professor = await Professor.findOne({ where: { id: professorId, adminId } });
+
+    if (!professor) {
+      throw ErrorHandler.notFound('Professor não encontrado');
     }
 
     await professor.remove();
