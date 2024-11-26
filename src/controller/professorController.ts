599f11b2061ae972804c928c6ea52298fcc06e1e
Agleice Faria de Sousa <146428762+agleicesousa@users.noreply.github.com>
2024-11-26 01:36:46 -0300

    refactor: criar professor

--- src/controller/professorController.ts
+++ src/controller/professorController.ts
@@ -1,109 +1,21 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { ProfessorService } from '../services/professorService';
-import ErrorHandler from '../errors/errorHandler';
 
 export class ProfessorController {
   private professorService = new ProfessorService();
 
-  async criarProfessor(req: Request, res: Response) {
-    const {
-      nomeMembro,
-      cpf,
-      dataNascimento,
-      numeroMatricula,
-      email,
-      senha,
-      turmaIds
-    } = req.body;
-    const adminId = req.user.id;
-
+  async criarProfessor(req: Request, res: Response, next: NextFunction) {
     try {
-      const professor = await this.professorService.criarProfessor(
-        nomeMembro,
-        cpf,
-        dataNascimento,
-        numeroMatricula,
-        email,
-        senha,
-        adminId,
-        turmaIds
+      const adminLogadoId = req.user?.id || null;
+
+      const novoProfessor = await this.professorService.criarProfessor(
+        req.body,
+        adminLogadoId
       );
-      return res.status(201).json(professor);
+
+      res.status(201).json(novoProfessor);
     } catch (error) {
-      if (error instanceof ErrorHandler) {
-        return res.status(error.statusCode).json({ message: error.message });
-      }
-      return res.status(500).json({ message: 'Erro interno do servidor' });
-    }
-  }
-
-  async listarProfessores(req: Request, res: Response) {
-    const adminId = req.user.id;
-
-    try {
-      const professores =
-        await this.professorService.listarProfessores(adminId);
-      return res.status(200).json(professores);
-    } catch (error) {
-      if (error instanceof ErrorHandler) {
-        return res.status(error.statusCode).json({ message: error.message });
-      }
-      return res.status(500).json({ message: 'Erro interno do servidor' });
-    }
-  }
-
-  async buscarProfessorPorId(req: Request, res: Response) {
-    const professorId = parseInt(req.params.id);
-    const adminId = req.user.id;
-
-    try {
-      const professor = await this.professorService.buscarProfessorPorId(
-        professorId,
-        adminId
-      );
-      return res.status(200).json(professor);
-    } catch (error) {
-      if (error instanceof ErrorHandler) {
-        return res.status(error.statusCode).json({ message: error.message });
-      }
-      return res.status(500).json({ message: 'Erro interno do servidor' });
-    }
-  }
-
-  async editarProfessor(req: Request, res: Response) {
-    const professorId = parseInt(req.params.id);
-    const adminId = req.user.id;
-    const { email, senha, turmaIds } = req.body;
-
-    try {
-      const professor = await this.professorService.editarProfessor(
-        professorId,
-        adminId,
-        email,
-        senha,
-        turmaIds
-      );
-      return res.status(200).json(professor);
-    } catch (error) {
-      if (error instanceof ErrorHandler) {
-        return res.status(error.statusCode).json({ message: error.message });
-      }
-      return res.status(500).json({ message: 'Erro interno do servidor' });
-    }
-  }
-
-  async deletarProfessor(req: Request, res: Response) {
-    const professorId = parseInt(req.params.id);
-    const adminId = req.user.id;
-
-    try {
-      await this.professorService.deletarProfessor(professorId, adminId);
-      return res.status(204).send();
-    } catch (error) {
-      if (error instanceof ErrorHandler) {
-        return res.status(error.statusCode).json({ message: error.message });
-      }
-      return res.status(500).json({ message: 'Erro interno do servidor' });
+      next(error);
     }
   }
 }
--- src/services/professorService.ts
+++ src/services/professorService.ts
@@ -4,141 +4,53 @@ import { Membros } from '../entities/membrosEntities';
 import { Turma } from '../entities/turmasEntities';
 import { TipoConta } from '../entities/baseEntity';
 import ErrorHandler from '../errors/errorHandler';
-import { criptografarSenha } from '../utils/senhaUtils';
 import { MysqlDataSource } from '../config/database';
 
 export class ProfessorService {
-  private professorRepository = MysqlDataSource.getRepository(Professor);
-  private membrosRepository = MysqlDataSource.getRepository(Membros);
-  private turmaRepository = MysqlDataSource.getRepository(Turma);
+  private async iniciarDatabase() {
+    if (!MysqlDataSource.isInitialized) {
+      await MysqlDataSource.initialize();
+    }
+  }
 
   async criarProfessor(
-    nomeMembro: string,
-    cpf: string,
-    dataNascimento: Date,
-    numeroMatricula: string,
-    email: string,
-    senha: string,
-    adminId: number,
-    turmaIds: number[]
-  ): Promise<Professor> {
-    // Verifica se o email já está registrado
-    const membroExistente = await this.membrosRepository.findOne({
-      where: { email }
+    dadosProfessor: {
+      email: string;
+      nomeCompleto: string;
+      numeroMatricula: string;
+      turmasId: number[];
+    },
+    adminCriadorId: number | null
+  ) {
+    await this.iniciarDatabase();
+
+    const membrosRepository = MysqlDataSource.getRepository(Membros);
+    const professorRepository = MysqlDataSource.getRepository(Professor);
+    const turmasRepository = MysqlDataSource.getRepository(Turma);
+
+    const turmas = await turmasRepository.find({
+      where: { id: In(dadosProfessor.turmasId) }
     });
-    if (membroExistente) {
-      throw ErrorHandler.badRequest('Email já registrado');
+
+    if (turmas.length !== dadosProfessor.turmasId.length) {
+      throw ErrorHandler.notFound('Algumas turmas não foram encontradas.');
     }
 
-    // Cria um novo membro
-    const membro = this.membrosRepository.create({
-      nomeCompleto: nomeMembro,
-      cpf,
-      dataNascimento,
-      numeroMatricula,
-      email,
-      senha: await criptografarSenha(senha),
-      tipoConta: TipoConta.PROFESSOR
-    });
-    await this.membrosRepository.save(membro);
-
-    // Busca as turmas para associar ao professor
-    const turmas = await this.turmaRepository.find({
-      where: { id: In(turmaIds) }
-    });
-    if (turmas.length !== turmaIds.length) {
-      throw ErrorHandler.notFound('Algumas turmas não foram encontradas');
-    }
-
-    // Cria o professor com o membro criado
-    const professor = this.professorRepository.create({
-      membro: membro,
-      turmas,
-      adminId
+    const membro = membrosRepository.create({
+      email: dadosProfessor.email,
+      nomeCompleto: dadosProfessor.nomeCompleto,
+      numeroMatricula: dadosProfessor.numeroMatricula,
+      tipoConta: TipoConta.PROFESSOR,
+      adminCriadorId: adminCriadorId ? { id: adminCriadorId } : null
     });
 
-    // Salva o professor no banco de dados
-    await this.professorRepository.save(professor);
+    await membrosRepository.save(membro);
 
-    return professor;
-  }
-
-  async editarProfessor(
-    professorId: number,
-    adminId: number,
-    email: string,
-    senha: string,
-    turmaIds: number[]
-  ): Promise<Professor> {
-    const professor = await this.professorRepository.findOne({
-      where: { id: professorId },
-      relations: ['admin', 'membro', 'turmas']
+    const professor = professorRepository.create({
+      membro,
+      turmas
     });
 
-    if (!professor) {
-      throw ErrorHandler.notFound('Professor não encontrado');
-    }
-
-    if (professor.admin.id !== adminId) {
-      throw ErrorHandler.forbidden(
-        'Você não tem permissão para editar esse professor'
-      );
-    }
-
-    if (email) professor.membro.email = email;
-    if (senha) professor.membro.senha = await criptografarSenha(senha);
-
-    const turmas = await this.turmaRepository.find({
-      where: { id: In(turmaIds) }
-    });
-    if (turmas.length !== turmaIds.length) {
-      throw ErrorHandler.notFound('Algumas turmas não foram encontradas');
-    }
-
-    professor.turmas = turmas;
-
-    await this.professorRepository.save(professor);
-    return professor;
-  }
-
-  async listarProfessores(adminId: number): Promise<Professor[]> {
-    const professores = await this.professorRepository.find({
-      where: { adminId },
-      relations: ['membro', 'turmas']
-    });
-
-    if (!professores.length) {
-      throw ErrorHandler.notFound('Nenhum professor encontrado');
-    }
-
-    return professores;
-  }
-
-  async buscarProfessorPorId(
-    professorId: number,
-    adminId: number
-  ): Promise<Professor> {
-    const professor = await this.professorRepository.findOne({
-      where: { id: professorId, adminId },
-      relations: ['membro', 'turmas']
-    });
-
-    if (!professor) {
-      throw ErrorHandler.notFound('Professor não encontrado');
-    }
-
-    return professor;
-  }
-
-  async deletarProfessor(professorId: number, adminId: number): Promise<void> {
-    const professor = await this.professorRepository.findOne({
-      where: { id: professorId, adminId }
-    });
-
-    if (!professor) {
-      throw ErrorHandler.notFound('Professor não encontrado');
-    }
-
-    await this.professorRepository.remove(professor);
+    return await professorRepository.save(professor);
   }
 }
