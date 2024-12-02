35ac7d86e5ffa8bf09d34635ba66c9e452e33afe
darlison-calm <bernardo.darlison@gmail.com>
2024-12-02 18:55:49 -0300

    feat: coluna desempenho em entidade Aluno
    - Adiciona coluna desempenho em entidade
    - Adicionar função que calcula o desempenho do aluno baseado nas médias de cada seção

--- src/entities/alunosEntities.ts
+++ src/entities/alunosEntities.ts
@@ -1,9 +1,25 @@
-import { Entity, ManyToOne, OneToOne, JoinColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  ManyToOne,
+  OneToOne,
+  JoinColumn,
+  OneToMany,
+  Column
+} from 'typeorm';
 import { BaseEntity } from './baseEntity';
 import { Membros } from './membrosEntities';
 import { Turma } from './turmasEntities';
 import { Admin } from './adminEntities';
-import { PDI } from './pdiEntities';
+import { PDI, PdiSecao } from './pdiEntities';
+
+const tabelaDeDesempenho = {
+  5: 'Exemplar',
+  4: 'Engajado',
+  3: 'Evolução',
+  2: 'Atenção',
+  1: 'Crítico'
+};
+
 @Entity('alunos')
 export class Alunos extends BaseEntity {
   @OneToOne(() => Membros, { eager: true })
@@ -22,4 +38,21 @@ export class Alunos extends BaseEntity {
     cascade: true
   })
   pdi: PDI[];
+
+  @Column({
+    type: 'varchar',
+    nullable: true
+  })
+  desempenho: string;
+
+  calcularDesempenho(secoes: PdiSecao[]): string {
+    const somaDesempenho = secoes.reduce(
+      (soma, secao) => soma + Number(secao.media),
+      0
+    );
+    console.log(somaDesempenho);
+    const mediaDesempenho = Math.floor(somaDesempenho / secoes.length);
+    // Garante que a chave fique dentro do intervalo de 1 a 5
+    return tabelaDeDesempenho[Math.min(Math.max(mediaDesempenho, 1), 5)];
+  }
 }
