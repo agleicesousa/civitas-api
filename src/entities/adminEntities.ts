e5b14c19cd1a9ddc038682992921f77510a8e719
agleicesousa <agleicesousa98@gmail.com>
2024-12-17 12:06:54 -0300

    doc: adminEntities.ts

--- src/entities/adminEntities.ts
+++ src/entities/adminEntities.ts
@@ -5,18 +5,35 @@ import { Turma } from './turmasEntities';
 import { Professor } from './professorEntities';
 import { Alunos } from './alunosEntities';
 
+/**
+ * Entidade principal para representar os administradores do sistema.
+ * Relaciona-se com membros, turmas, professores e alunos.
+ */
 @Entity('admin')
 export class Admin extends BaseEntity {
+  /**
+   * Relacionamento OneToOne com a entidade Membros.
+   * A exclusão em cascata garante que se apagar um membro, o administrador associado também seja excluído.
+   */
   @OneToOne(() => Membros, { cascade: true, onDelete: 'CASCADE' })
   @JoinColumn({ name: 'membroId' })
   membro: Membros;
 
+  /**
+   * Relacionamento OneToMany com as turmas administradas pelo administrador.
+   */
   @OneToMany(() => Turma, (turma) => turma.admin)
   turmas: Turma[];
 
+  /**
+   * Relacionamento OneToMany com os professores sob a administração do administrador.
+   */
   @OneToMany(() => Professor, (professor) => professor.admin)
   professores: Professor[];
 
+  /**
+   * Relacionamento OneToMany com os alunos sob a administração do administrador.
+   */
   @OneToMany(() => Alunos, (aluno) => aluno.admin)
   alunos: Alunos[];
 }
