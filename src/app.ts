bedd9c108cf9f47dee34c33173b07d1d268d27d6
agleicesousa <agleicesousa98@gmail.com>
2024-12-17 13:11:56 -0300

    doc: app

--- src/app.ts
+++ src/app.ts
@@ -13,34 +13,51 @@ import alunoRouter from './routes/alunoRoutes';
 import loginRouter from './routes/loginRoutes';
 import pdiRouter from './routes/pdiRoutes';
 
+/**
+ * Função para inicializar a conexão com o banco de dados.
+ */
 const initializeDatabase = async () => {
   try {
     await MysqlDataSource.initialize();
     console.log('✅ Database initialized!');
   } catch (err) {
     console.error('❌ Database initialization failed:', err);
-    process.exit(1);
+    process.exit(1); // Encerra o servidor caso a conexão com o banco falhe.
   }
 };
 
+/**
+ * Configuração do Swagger para documentação da API.
+ * @param app Express application instance.
+ */
 const setupSwagger = (app: express.Application) => {
   const swaggerSpec = swaggerJSDoc(swaggerConfig);
+
+  // Configurações de Swagger UI.
   app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
   app.get('/swagger.json', (_req, res) => res.send(swaggerSpec));
   console.log('✅ Swagger configured at /swagger');
 };
 
+/**
+ * Função principal que configura e inicializa o servidor Express.
+ */
 const main = async () => {
   await initializeDatabase();
 
   const app = express();
 
+  // Configurações básicas para JSON e CORS.
   app.use(express.json());
   app.use(cors({ origin: true }));
+
+  // Configura o tratamento global de erros da aplicação.
   app.use(errorHandler);
 
+  // Configurações do Swagger.
   setupSwagger(app);
 
+  // Configuração de rotas.
   app.use('/admin', adminRouter);
   app.use('/membros', membrosRouter);
   app.use('/turmas', turmasRouter);
@@ -49,15 +66,18 @@ const main = async () => {
   app.use('/alunos', alunoRouter);
   app.use('/auth', loginRouter);
 
+  // Define a porta do servidor.
   const PORT = process.env.SERVER_PORT || 3000;
   if (!PORT) {
     console.error('❌ SERVER_PORT não definido nas variáveis de ambiente.');
     process.exit(1);
   }
 
+  // Inicia o servidor na porta configurada.
   app.listen(PORT, () => {
     console.log(`✅ Server listening on port ${PORT}`);
   });
 };
 
+// Executa a função principal.
 main();
