af3f40f803c3bae25ee3043e603348ec0fa25a74
agleicesousa <agleicesousa98@gmail.com>
2024-12-17 11:58:12 -0300

    doc: baseEntity.ts

--- src/entities/baseEntity.ts
+++ src/entities/baseEntity.ts
@@ -5,19 +5,35 @@ import {
   BeforeUpdate
 } from 'typeorm';
 
+/**
+ * Enum representando os tipos de conta no sistema.
+ */
 export enum TipoConta {
   ADMIN = 'admin',
   PROFESSOR = 'professor',
   ALUNO = 'aluno'
 }
 
+/**
+ * Classe base para todas as entidades com campos comuns como `id`, `dataCriacao` e `dataAtualizacao`.
+ * Inclui hooks para atualizar automaticamente as datas de criação e atualização.
+ */
 export abstract class BaseEntity {
+  /** Identificador único gerado automaticamente para cada entidade. */
   @PrimaryGeneratedColumn()
   id: number;
 
+  /**
+   * Data de criação da entidade.
+   * Configurada automaticamente no momento da criação.
+   */
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   dataCriacao: Date;
 
+  /**
+   * Data de última atualização da entidade.
+   * Atualizada automaticamente sempre que houver uma modificação.
+   */
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
@@ -25,11 +41,19 @@ export abstract class BaseEntity {
   })
   dataAtualizacao: Date;
 
+  /**
+   * Hook executado antes da inserção de um registro no banco de dados.
+   * Configura o campo `dataCriacao` com a data atual.
+   */
   @BeforeInsert()
   setDataCriacao(): void {
     this.dataCriacao = new Date();
   }
 
+  /**
+   * Hook executado antes da atualização de um registro no banco de dados.
+   * Configura o campo `dataAtualizacao` com a data atual.
+   */
   @BeforeUpdate()
   setDataAtualizacao(): void {
     this.dataAtualizacao = new Date();
