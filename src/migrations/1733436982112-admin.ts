e8569e5df6493e3611ae9e60f6f30c7b4fe00a70
agleicesousa <agleicesousa98@gmail.com>
2024-12-17 12:19:17 -0300

    doc: migrations

--- src/migrations/1733436982112-admin.ts
+++ src/migrations/1733436982112-admin.ts
@@ -1,6 +1,18 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
+/**
+ * Migration para criar a tabela 'admin' no banco de dados.
+ *
+ * A tabela 'admin' é usada para armazenar informações de administradores no sistema,
+ * incluindo dados básicos, autenticação e o tipo de conta.
+ */
 export class Admin1733436982112 implements MigrationInterface {
+  /**
+   * Método executado para aplicar a migração (criar tabela).
+   * Verifica se a tabela já existe antes de criá-la para evitar erros no ambiente de migração.
+   *
+   * @param queryRunner - O objeto responsável por executar as operações no banco de dados.
+   */
   public async up(queryRunner: QueryRunner): Promise<void> {
     const tableExists = await queryRunner.hasTable('admin');
     if (!tableExists) {
@@ -54,6 +66,12 @@ export class Admin1733436982112 implements MigrationInterface {
     }
   }
 
+  /**
+   * Método executado para desfazer a migração (roll back).
+   *
+   * No caso dessa migração, nenhuma ação é realizada no método `down`.
+   * É utilizado apenas para manter consistência em operações futuras.
+   */
   public async down(): Promise<void> {
     console.log('Nenhuma ação executada no down.');
   }
--- src/migrations/1733439199040-insertAdmin.ts
+++ src/migrations/1733439199040-insertAdmin.ts
@@ -3,7 +3,21 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 import 'dotenv/config';
 import { criptografarSenha } from '../utils/validarSenhaUtils';
 
+/**
+ * Migration responsável por inserir um usuário administrador padrão no banco de dados.
+ * Essa ação é executada caso as tabelas necessárias existam no banco.
+ *
+ * A senha é criptografada antes de ser salva.
+ */
 export class InsertAdmin1733439199040 implements MigrationInterface {
+  /**
+   * Método executado para aplicar a migração.
+   * Insere um usuário administrador no banco de dados com as credenciais definidas em variáveis de ambiente.
+   *
+   * Verifica se as tabelas necessárias existem e procede com a criação do usuário.
+   *
+   * @param queryRunner - O objeto responsável por realizar as operações no banco de dados.
+   */
   public async up(queryRunner: QueryRunner): Promise<void> {
     const tabelaMembros = await queryRunner.hasTable('membros');
     const tabelaAdmin = await queryRunner.hasTable('admin');
@@ -32,6 +46,11 @@ export class InsertAdmin1733439199040 implements MigrationInterface {
     }
   }
 
+  /**
+   * Método executado para desfazer a migração.
+   * Atualmente, nenhuma ação é executada no método `down`.
+   * No caso de implementação futura, você poderia apagar o administrador adicionado.
+   */
   public async down(): Promise<void> {
     console.log('Nenhuma ação executada no down.');
   }
