2b1da71f17f34f852d6033c4897aa7358919e6de
agleicesousa <agleicesousa98@gmail.com>
2025-01-03 15:10:59 -0300

    refactor: remover configuração de segurança BearerAuth do swagger

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -31,12 +31,7 @@ export const swaggerConfig: swaggerJSDoc.Options = {
           bearerFormat: 'JWT'
         }
       }
-    },
-    security: [
-      {
-        BearerAuth: []
-      }
-    ]
+    }
   },
   apis: ['src/routes/*.ts', 'routes/*.js']
 };
